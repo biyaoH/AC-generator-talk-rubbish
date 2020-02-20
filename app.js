abb4396a935ddd7c79da17b5aaefc5e3a8837b48
Yao <biyao.hsu@gmail.com>
2020-02-20 15:38:52 +0100

    add generate_talk_rubbish.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateTalkRubbish = require('./generate_talk_rubbish')
 const app = express()
 
 const port = 3000
@@ -7,30 +9,19 @@ const port = 3000
 app.engine('handlebars', exphbs({ degaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  const task = {
-    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
-    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
 
-  }
-
-  const phrase = ['很簡單', '很容易', '很快', '很正常']
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  console.log('get form POST request')
-  const task = {
-    engineer: ['加個按鈕', '加新功能', '切個版', '改一點 code'],
-    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
-    entrepreneur: ['週末加班', '要能賺錢', '想個 business model', '找 VC 募錢']
-
-  }
-
-  const phrase = ['很簡單', '很容易', '很快', '很正常']
-  res.render('index')
+  // console.log('req.body', req.body)
+  console.log(generateTalkRubbish(req.body.profession))
+  const talkRubbish = generateTalkRubbish(req.body.profession)
+  res.render('index', { talkRubbish: talkRubbish })
 })
 
 app.listen(port, () => {
---
+++ generate_talk_rubbish.js
@@ -0,0 +1,30 @@
+function getRandomItem(arr) {
+  const index = Math.floor(Math.random() * arr.length)
+  return arr[index]
+}
+
+function generateTalkRubbish(profession) {
+  // profession = 'engineer'
+  // console.log(profession)
+  const professionName = {
+    engineer: '工程師',
+    designer: '設計師',
+    entrepreneur: '創業家'
+  }
+
+  const task = {
+    engineer: ['加個按鈕', '加新功能', '切個版', '改一點code'],
+    designer: ['畫一張圖', '改個 logo', '順便幫忙設計一下', '隨便換個設計'],
+    entrepreneur: ['週末加班', '要能賺錢', '想個business model', '找VC 募錢']
+  }
+
+  const phrase = ['很簡單', '很容易', '很快', '很正常']
+  // console.log(task[profession])
+  const sentence = '身為一個' + professionName[profession] + getRandomItem(task[profession]) + getRandomItem(phrase)
+
+  console.log(sentence)
+  return sentence
+
+}
+
+module.exports = generateTalkRubbish
